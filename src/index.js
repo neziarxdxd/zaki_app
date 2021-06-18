659ae6b56dd42082a396b75681d623ae9b63fcb7
zenraiching <rickdavidsangalang@gmail.com>
2021-06-18 18:08:25 +0800

    add

--- src/index.js
+++ src/index.js
@@ -1,44 +1,6 @@
 const { router, route,text } = require('bottender/router');
 const fetch = require('node-fetch');
-
-async function doSolveThis(context){
-
-  //get the text
-  var parseString = context.event.text
-
-  // this will split the string by space 
-  var splittedString = parseString.split(" ")
-
-  // removes the forward slash and get the function 
-  var command = splittedString[0].substring(1,)
-
-  // this get the whole equation 
-  var eq = splittedString.slice(1, splittedString.length )  
-  var equation= eq.join(" ");
-
-  // send the Command and Equation 
-  await context.sendText("command: "+command)
-  await context.sendText("equation: "+equation)
-  
-    
-  // transform the equation to URI
-  var encodedUrl = encodeURIComponent(equation)
-  
-  try {
-    // fetch dsds
-    var response = await fetch(`https://newton.now.sh/api/v2/${command}/${encodedUrl}`)
-    var jsonBlocks = await response.json()
-    await context.sendText(`result: ${jsonBlocks.result}`)
-  }  
-  catch (e) {    
-    console.error(e)    
-  }
- 
-}
-
-async function sendHelp(context){
-  await context.sendText("Hi kindly choose the some  commands ") 
-}
+const google = require('./google/google');
 
 async function wikiPediaSend(context){
   var parseString = context.event.text
@@ -64,46 +26,33 @@ async function wikiPediaSend(context){
   }
 }
 
-async function dictionarySend(context){
-  var parseString = context.event.text
-
-  // this will split the string by space 
-  var splittedString = parseString.split(" ") 
-
-  // this get the whole equation 
-  var eq = splittedString.slice(1, splittedString.length )  
-  var searchWord= eq.join(" ");
-
-  try {
-    // fetch dsds
-    var response = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${searchWord}`)
-    var jsonBlocks = await response.json();
-     var temporaryData = jsonBlocks[0]["meanings"]    
-    await context.sendText(temporaryData[0]["definitions"][0]["definition"])
-  }  
-  catch (e) {    
-    console.error(e)    
-  }
-}
-
-
 module.exports = async function App(context) {
-  return router(
-    [     
-      text(/^\/simplify[]?\s+/,doSolveThis),  
-      text(/^\/factor[]?\s+/,doSolveThis),  
-      text(/^\/derive[]?\s+/,doSolveThis),  
-      text(/^\/integrate[]?\s+/,doSolveThis),  
-      text(/^\/zeroes[]?\s+/,doSolveThis),  
-      text(/^\/tangent[]?\s+/,doSolveThis),  
-      text(/^\/area[]?\s+/,doSolveThis),  
-      text(/^\/cos[]?\s+/,doSolveThis),
-      text(/^\/sin[]?\s+/,doSolveThis),
-      text(/^\/log[]?\s+/,doSolveThis),  
-      text(/^\/wiki[]?\s+/,wikiPediaSend),  
-      text(/^\/word[]?\s+/,dictionarySend),  
-      route('*',sendHelp)
-    ]
+ 
+  var number = context.state.number; 
+  if(context.state.wiki){
+    context.setState({wiki:false})
+    await context.sendText(`Here are the things about ${context.event.text}`)
+  }
+  
+  if (number=="1") {
+    context.resetState(); 
+    context.setState({wiki:true});
+    await context.sendText(`Send me what you want to search using wikipedia`);
+  }
 
-  );
+  else if (number=="2") {  
+    context.resetState();  
+    await context.sendText(`Send me what you want to search using dictionary`);
+  }
+  else if (number=="3") { 
+    context.resetState();   
+    await context.sendText(`Send me your question`);
+  }
+  else{
+    context.resetState();
+    context.setState({ number: context.event.text });
+    await context.sendText("Choose among the numbers 1,2,3 ?");
+  }
+
+  
 };
